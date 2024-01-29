39b96d3e158ec10e1cccce9daf72d55cbfd6645b
Valyo96 <107226915+Valyo96@users.noreply.github.com>
2024-01-29 19:01:35 +0200

    test github integration with slack

--- src/main/java/com/ludogoriesoft/job/crawler/JobCrawler/company/persistence/CompanyRepository.java
+++ src/main/java/com/ludogoriesoft/job/crawler/JobCrawler/company/persistence/CompanyRepository.java
@@ -3,4 +3,5 @@ package com.ludogoriesoft.job.crawler.JobCrawler.company.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
 }
