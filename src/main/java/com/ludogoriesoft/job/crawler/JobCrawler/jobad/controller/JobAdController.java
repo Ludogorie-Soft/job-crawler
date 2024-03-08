6620424c299130128af7f311a4a0524d63a1bb1c
civelin <tsvetelinaivelin@gmail.com>
2024-03-08 14:58:45 +0200

    add updating for job ad

--- src/main/java/com/ludogoriesoft/job/crawler/JobCrawler/jobad/controller/JobAdController.java
+++ src/main/java/com/ludogoriesoft/job/crawler/JobCrawler/jobad/controller/JobAdController.java
@@ -1,11 +1,12 @@
 package com.ludogoriesoft.job.crawler.JobCrawler.jobad.controller;
 
+import com.ludogoriesoft.job.crawler.JobCrawler.jobad.persistance.JobAdStatus;
 import com.ludogoriesoft.job.crawler.JobCrawler.jobad.service.JobAdService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 @RequestMapping("/job-ad")
@@ -17,6 +18,16 @@ public class JobAdController {
     @GetMapping("/all")
     public String getAll(Model model) {
         model.addAttribute("ads", service.list());
+        model.addAttribute("statuses", JobAdStatus.values());
         return "job-ads";
     }
+
+    @PostMapping("/update/{id}")
+    public String updateStatus(@PathVariable Long id, @RequestParam("newStatus") String newStatus) {
+        System.out.println(id);
+        System.out.println(newStatus);
+         service.updateStatus(id, newStatus);
+        return "redirect:/job-ad/all";
+    }
+
 }
