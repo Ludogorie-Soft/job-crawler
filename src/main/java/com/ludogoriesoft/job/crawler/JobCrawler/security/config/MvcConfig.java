fe8d980a4dc7079a78b21acba6f4818b6e0cd0d3
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2024-04-08 19:37:29 +0300

    add home and login page

--- src/main/java/com/ludogoriesoft/job/crawler/JobCrawler/security/config/MvcConfig.java
+++ src/main/java/com/ludogoriesoft/job/crawler/JobCrawler/security/config/MvcConfig.java
@@ -1,6 +1,5 @@
 package com.ludogoriesoft.job.crawler.JobCrawler.security.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,9 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
 }
